a684db2c3fd527b8dcaa2febf641dcfc2ed7344a
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-07-15 09:36:26 +0000

    ﾕｰｻﾞｰ編集へのｱｸｾｽ制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
     @user = User.find(params[:id])
   end
 
@@ -17,13 +21,17 @@ class UsersController < ApplicationController
   end
 
   def update
-     @user = User.find(params[:id])
-     if @user.update(user_params)
-       flash[:notice] = "You have updated user successfully."
-       redirect_to user_path(@user.id)
-     else
-       render 'edit'
-     end
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path
+    end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
   end
 
   private
