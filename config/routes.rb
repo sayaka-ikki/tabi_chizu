f85f5f3ce43807fbf96a5e8903c148631149e922
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-07-29 16:04:42 +0000

    管理者機能-ユーザー削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "homes#top"
 
